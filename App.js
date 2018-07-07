57aab1b0a20bea60eb42121b402224dbc842f04b
Nicos <nick.zonias@gmail.com>
2018-07-07 19:57:15 +0100

    add linear gradient module

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, Animated } from 'react-native';
 
+import { DangerZone } from 'expo';
+const { Lottie } = DangerZone;
+
 import { API_KEY } from './utils/WeatherAPIKey';
 
 import Weather from './components/Weather';
 
 export default class App extends React.Component {
   state = {
-    isLoading: false,
+    isLoading: true,
     temperature: 0,
     weatherCondition: null,
     error: null
@@ -16,23 +19,22 @@ export default class App extends React.Component {
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+        this.fetchWeahter(position.coords.latitude, position.coords.longitude);
       },
       error => {
         this.setState({
-          error: 'Error Gettig Weather Condtions'
+          error: 'Error Getting Weather Condtions'
         });
       }
     );
   }
 
-  fetchWeather(lat = 25, lon = 25) {
+  fetchWeahter(lat, lon) {
     fetch(
       `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
     )
       .then(res => res.json())
       .then(json => {
-        // console.log(json);
         this.setState({
           temperature: json.main.temp,
           weatherCondition: json.weather[0].main,
@@ -42,10 +44,16 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, weatherCondition, temperature } = this.state;
     return (
       <View style={styles.container}>
-        {isLoading ? <Text>Fetching Weather Info...</Text> : <Weather />}
+        {isLoading ? (
+          <View style={styles.loadingContainer}>
+            <Text style={styles.loadingText}>Fetching Weather Info...</Text>
+          </View>
+        ) : (
+          <Weather weather={weatherCondition} temperature={temperature} />
+        )}
       </View>
     );
   }
@@ -55,5 +63,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
+  },
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFFDE4'
+  },
+  loadingText: {
+    fontSize: 30
   }
 });
--- components/Weather.js
+++ components/Weather.js
@@ -1,24 +1,68 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import PropTypes from 'prop-types';
+import { weatherConditions } from '../utils/WeatherConditions';
 
-const Weather = () => {
+const Weather = ({ weather, temperature }) => {
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer} />
-      <View style={styles.bodyContainer} />
+    <View
+      style={[
+        styles.weatherContainer,
+        { backgroundColor: weatherConditions[weather].color }
+      ]}
+    >
+      <Text style={styles.tempText}>{temperature}˚</Text>
+      <View style={styles.headerContainer}>
+        <MaterialCommunityIcons
+          size={200}
+          name={weatherConditions[weather].icon}
+          color={'#fff'}
+        />
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text style={styles.title}>{weatherConditions[weather].title}</Text>
+        <Text style={styles.subtitle}>
+          {weatherConditions[weather].subtitle}
+        </Text>
+      </View>
     </View>
   );
 };
 
+Weather.propTypes = {
+  temperature: PropTypes.number.isRequired,
+  weather: PropTypes.string
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
+  weatherContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    padding: 40
   },
   headerContainer: {
-    flex: 1
+    padding: 0
+  },
+  tempText: {
+    paddingLeft: 20,
+    fontSize: 72,
+    color: '#fff'
   },
   bodyContainer: {
-    flex: 1
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+    marginBottom: 40
+  },
+  title: {
+    fontSize: 60,
+    color: '#fff'
+  },
+  subtitle: {
+    fontSize: 24,
+    color: '#fff'
   }
 });
 
