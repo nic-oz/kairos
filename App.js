ee9760ad965869d5b7ded4f042207b6369f46778
Nicos <nick.zonias@gmail.com>
2018-07-07 18:26:24 +0100

    add fetch weather data function

--- App.js
+++ App.js
@@ -1,51 +1,54 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-const Weather = () => {
-  return (
-    <View style={styles.weatherContainer}>
-      <View style={styles.headerContainer}>
-        <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
-        <Text style={styles.tempText}>Temperature˚</Text>
+import { API_KEY } from './utils/WeatherAPIKey';
+
+import Weather from './components/Weather';
+
+export default class App extends React.Component {
+  state = {
+    isLoading: false,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error Gettig Weather Condtions'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat = 25, lon = 25) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    )
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+      });
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoading ? <Text>Fetching Weather Info...</Text> : <Weather />}
       </View>
-      <View style={styles.bodyContainer}>
-        <Text style={styles.title}>So Sunny</Text>
-        <Text style={styles.subtitle}>It hurts my eyes!</Text>
-      </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  weatherContainer: {
+  container: {
     flex: 1,
-    backgroundColor: '#f7b733'
-  },
-  headerContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  tempText: {
-    fontSize: 48,
-    color: '#fff'
-  },
-  bodyContainer: {
-    flex: 2,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-end',
-    paddingLeft: 25,
-    marginBottom: 40
-  },
-  title: {
-    fontSize: 48,
-    color: '#fff'
-  },
-  subtitle: {
-    fontSize: 24,
-    color: '#fff'
+    backgroundColor: '#fff'
   }
 });
-
-export default Weather;
