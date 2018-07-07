2ef4913ace4efdf44272218b338a070ed90e6cce
Nicos <nick.zonias@gmail.com>
2018-07-07 18:17:41 +0100

    add basic styling and icons to weather component

--- App.js
+++ App.js
@@ -1,32 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default class App extends React.Component {
-  state = {
-    isLoading: false
-  };
-
-  render() {
-    const { isLoading } = this.state;
-    return (
-      <View style={styles.container}>
-        {isLoading ? (
-          <Text> Fetching Weather Info...</Text>
-        ) : (
-          <View>
-            <Text>Kairos Weather App</Text>
-          </View>
-        )}
+const Weather = () => {
+  return (
+    <View style={styles.weatherContainer}>
+      <View style={styles.headerContainer}>
+        <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
+        <Text style={styles.tempText}>Temperature˚</Text>
       </View>
-    );
-  }
-}
+      <View style={styles.bodyContainer}>
+        <Text style={styles.title}>So Sunny</Text>
+        <Text style={styles.subtitle}>It hurts my eyes!</Text>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
+  weatherContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f7b733'
+  },
+  headerContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  tempText: {
+    fontSize: 48,
+    color: '#fff'
+  },
+  bodyContainer: {
+    flex: 2,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+    paddingLeft: 25,
+    marginBottom: 40
+  },
+  title: {
+    fontSize: 48,
+    color: '#fff'
+  },
+  subtitle: {
+    fontSize: 24,
+    color: '#fff'
   }
 });
+
+export default Weather;
